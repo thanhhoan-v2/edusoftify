c17f581cd6e5d39589143d42aa5a7737890ce29d
thanhhoann <pdth456@gmail.com>
2024-10-12 21:58:59 +0700

    fix: disable zoom on ios safari

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,7 +132,7 @@ export default function Home() {
 			<Head>
 				<meta
 					name="viewport"
-					content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+					content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
 				/>
 			</Head>
 
